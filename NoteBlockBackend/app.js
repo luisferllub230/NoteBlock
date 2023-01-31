85be8a99eb709a9960e906202fc675cbdd680146
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2023-01-31 01:16:29 -0400

    update in noteblock project backend and frontend

--- NoteBlockBackend/app.js
+++ NoteBlockBackend/app.js
@@ -3,10 +3,12 @@ import express  from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`http://localhost:${port}`);
 });
