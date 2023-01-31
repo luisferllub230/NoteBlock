dea3777399e5d14ada98a9a8ad15618d5cc362bd
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2023-01-31 01:38:02 -0400

    update rename

--- NoteBlock-angular/karma.conf.js
+++ NoteBlock-angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/note-block'),
+      dir: require('path').join(__dirname, './coverage/note-block-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- NoteBlock-angular/src/app/app.component.spec.ts
+++ NoteBlock-angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NoteBlock'`, () => {
+  it(`should have as title 'NoteBlock-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NoteBlock');
+    expect(app.title).toEqual('NoteBlock-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NoteBlock app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('NoteBlock-angular app is running!');
   });
 });
