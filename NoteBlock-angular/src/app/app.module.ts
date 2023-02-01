d858ffec183f0e56583df5969f45fcf3e4941af5
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2023-02-01 19:11:03 -0400

    NOTBLO-7 - crear un header - se creo un header y se aplico el diseno dado

--- NoteBlock-angular/src/app/app.module.ts
+++ NoteBlock-angular/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { LoggingComponent } from './auth/logging/logging.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { Page404Component } from './page/page404/page404.component';
+import { HeaderComponent } from "./shared/header/header.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
+        ContactComponent,
+        LoggingComponent,
+        RegisterComponent,
+        FooterComponent,
+        Page404Component,
+        HeaderComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ]
 })
 export class AppModule { }
---
+++ NoteBlock-angular/src/app/auth/logging/logging.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logging',
+  templateUrl: './logging.component.html',
+  styleUrls: ['./logging.component.less']
+})
+export class LoggingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
